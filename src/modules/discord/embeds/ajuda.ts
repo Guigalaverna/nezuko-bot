032374aaf9cb143d2dadbf53eb4930f7dfaa755a
Guigalaverna <guigalaverna@gmail.com>
2022-05-01 19:16:55 -0300

    feat: rename 'material' to 'livro'

--- src/modules/discord/embeds/ajuda.ts
+++ src/modules/discord/embeds/ajuda.ts
@@ -8,7 +8,16 @@ export const embed = new MessageEmbed({
   thumbnail: {
     url: "https://c.tenor.com/t9Lbd0mHAUsAAAAd/nezuko-demon-slayer.gif",
   },
-  description: `Tá perdido? Eu vou te ajudar!\n\n**Comandos**:\n**/upload**: Caso queria enviar um material para a biblioteca.\n**/pesquisar**: Pesquisar algum material da biblioteca.\n**/ajuda**: Mostra essa mensagem linda.\n\nAcho que isso ajuda né?`,
+  description: `Tá perdido? Eu vou te ajudar!
+  
+  Todos os comandos sempre deve começar com n! e deve estar em minúsculo <3.
+
+  **Comandos**:
+  **n!ajuda**: Mostra essa mensagem linda.
+  **n!upload <idioma> <categoria> <nome do livro>**: Caso queria enviar um livro para a biblioteca.
+  **n!pesquisar <termo de pesquisa>**: Pesquisar algum material da biblioteca.
+  
+  Acho que isso ajuda né?`,
   color: "#D98BBD",
   image: {
     url: "https://pa1.narvii.com/7666/99b242f85f5d07a5c797618664b53d79db5e5986r1-512-288_hq.gif",
