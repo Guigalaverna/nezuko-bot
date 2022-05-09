44e95bb01e427a62b6d724631cac3095412215a3
Guigalaverna <guigalaverna@gmail.com>
2022-05-09 17:40:16 -0300

    feat: create database adapter

--- @types/adapters/DatabaseAdapter.d.ts
+++ @types/adapters/DatabaseAdapter.d.ts
@@ -1,5 +1,13 @@
+import { Book } from "@prisma/client";
+
 export interface DatabaseAdapter {
+  // search
   findBookByTitle(title: string): Promise<Book | null>;
-  findBookByAuthor(author: string): Promise<Book | null>;
-  findBookByPublisher(publisher: string): Promise<Book | null>;
+  findCategoryByName(name: string): Promise<Category | null>;
+
+  // create
+  createBook(book: CreateBook): Promise<Book>;
+
+  // create category
+  createCategory(category: CreateCategory): Promise<Category>;
 }
